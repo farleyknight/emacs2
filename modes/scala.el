26f3bc9e851039000ae9bef52fe6cf581564732d
Farley Knight <farleyknight@gmail.com>
2014-05-14 15:05:57 -0400

    Switching back to scala-mode as scala-mode2 isn't quite working at the moment

--- modes/scala.el
+++ modes/scala.el
@@ -1,7 +1,7 @@
 
-(add-to-list 'load-path "~/dev/emacs2/modes/scala-mode2")
+(add-to-list 'load-path "~/dev/emacs2/modes/scala-mode")
 
-(autoload 'scala-mode "scala-mode2" "Major mode for Scala." t)
+(autoload 'scala-mode "scala-mode" "Major mode for Scala." t)
 
 (add-to-list 'auto-mode-alist '("\\.scala$"  . scala-mode))
 (add-to-list 'auto-mode-alist '("build.sbt$" . scala-mode))
