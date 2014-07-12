608e325d3910675dba08a80426c72e578884fdb9
Farley Knight <farleyknight@gmail.com>
2014-07-12 12:11:48 -0400

    Demote useless ?char ruby syntax

--- functionality/color-theme.el
+++ functionality/color-theme.el
@@ -2,6 +2,10 @@
 (add-to-list 'load-path "~/dev/emacs2/functionality/color-theme-6.6.0/")
 
 (require 'color-theme)
+
+(color-theme-initialize)
+(color-theme-hober)
+
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
